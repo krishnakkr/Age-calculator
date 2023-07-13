487abd4a626e48ede9609a47fe9f3286dcef1cda
kakarla sai krishna <krishnakkr9999@gmail.com>
2023-07-13 18:56:15 +0530

    Update script.js for far years

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 let userInput = document.getElementById("date");
-userInput.max = new Date().toISOString().split("T")[0];
+userInput.max = moment().format("YYYY-MM-DD");
 let result = document.getElementById("result");
 function calculateAge() {
-    let birthDate = new Date(userInput.value);
-    let d1 = birthDate.getDate();
-    let m1 = birthDate.getMonth() + 1;
-    let y1 = birthDate.getFullYear();
+    let birthDate = moment(userInput.value, "YYYY-MM-DD");
+    let today = moment();
 
-    let today = new Date();
-    let d2 = today.getDate();
-    let m2 = today.getMonth() + 1;
-    let y2 = today.getFullYear();
+    let years = today.diff(birthDate, "years");
+    birthDate.add(years, "years");
 
-    let d3, m3, y3;
+    let months = today.diff(birthDate, "months");
+    birthDate.add(months, "months");
 
-    y3 = y2 - y1;
-    m3 = m2 - m1;
-    d3 = d2 - d1;
-
-    if (d3 < 0) {
-        m3--;
-        const daysInPrevMonth = getDaysInMonth(y1, m1);
-        d3 = daysInPrevMonth + d3;
-    }
-
-    if (m3 < 0) {
-        y3--;
-        m3 = 12 + m3;
-    }
+    let days = today.diff(birthDate, "days");
 
     result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months,<span>${d3}</span> days old`;
 }
